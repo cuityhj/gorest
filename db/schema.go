16fb07db35f33e2c58d460d287ae2033db12b9a0
yuhaijiao <yuhj@linkingthing.com>
2025-02-28 13:28:04 +0000

    fix drop schema for placeholder syntax error

--- db/schema.go
+++ db/schema.go
@@ -2,18 +2,19 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const (
-	dropSchemaSql   = "drop schema if exists $1 cascade"
+	dropSchemaSql   = "drop schema if exists %s cascade"
 	createSchemaSql = "create schema if not exists gr"
 )
 
 func InitSchema(pool *pgxpool.Pool, dropSchemaList ...string) error {
 	for _, schemaName := range dropSchemaList {
-		if _, err := pool.Exec(context.TODO(), dropSchemaSql, schemaName); err != nil {
+		if _, err := pool.Exec(context.TODO(), fmt.Sprintf(dropSchemaSql, schemaName)); err != nil {
 			return err
 		}
 	}
